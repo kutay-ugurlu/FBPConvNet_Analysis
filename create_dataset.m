a04371b484d8c98d78b729d0d55dd6368b7d2ad6
Kutay <kutay.ugurlu.1@gmail.com>
2023-01-18 17:18:31 +0300

    net results

--- create_dataset.m
+++ create_dataset.m
@@ -1,10 +1,9 @@
 wipe
 Data = struct2array(load("Cropped_Synapse_Abdomen_Data.mat"));
-slice_data = size(Data,4);
 n_views = 1000;
-subsampling_factor = 20;
+subsampling_factor = 5;
 if_noise = 1;
-noise = 30; %dB:
+noise = 20; %dB:
 save_file_name = ['N_',num2str(n_views),'_SS_',num2str(subsampling_factor),'_Noise_',num2str(if_noise),'_SNR_',num2str(noise),'.mat'];
 
 [SS_array,GT_array] = radon_helper(Data,n_views,subsampling_factor,if_noise,noise);
